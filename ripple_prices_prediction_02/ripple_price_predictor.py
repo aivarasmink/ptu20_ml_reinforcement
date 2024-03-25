e346c402214816f45cdd1ebb5515509056451570
aivarasmink <aivarasmink@gmail.com>
2024-03-25 15:35:23 +0200

    rasau koda reikia dokumentacija dar paskaityti

--- ripple_prices_prediction_02/ripple_price_predictor.py
+++ ripple_prices_prediction_02/ripple_price_predictor.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
+from sklearn.preprocessing import MinMaxScaler
 from keras.models import load_model
 from datetime import datetime
 
@@ -20,3 +21,40 @@ xrp_data = yf.download(stock, start, end)
 model = load_model("xrp_price_predictor.keras")
 st.subheader("XRP Data")
 st.write(xrp_data)
+
+splitting_len = int(len(xrp_data) * 0.9)
+x_test = pd.DataFrame(xrp_data.iloc[splitting_len:])
+
+st.subheader("Original Close price")
+figsize = (15, 6)
+fig = plt.figure(figsize=figsize)
+plt.plot(xrp_data["Close"],'b')
+st.pyplot(fig)
+
+st.subheader('Test Close price')
+st.write(x_test)
+
+st.subheader("Test Close price")
+figsize = (15, 6)
+fig = plt.figure(figsize=figsize)
+plt.plot(x_test["Close"],'b')
+st.pyplot(fig)
+
+
+# preprocessing data
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaler_data = scaler.fit_transform(x_test[['Close']].values)
+
+x_data = []
+y_data = []
+for i in range(100, len(scaler_data)):
+    x_data.append(scaler_data[i-100:i, 0])
+    y_data.append(scaler_data[i, 0])
+
+x_data, y_data = np.array(x_data), np.array(y_data)
+
+predictions = model.predict(x_data)
+inverted_predictions = scaler.inverse_transform(predictions)
+inverted_y_test = scaler.inverse_transform(y_data)
+
+
